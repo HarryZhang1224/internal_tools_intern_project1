85decca6bf6e437971efbf11e13b08f020cb3432
HarryZhang1224 <77279326+HarryZhang1224@users.noreply.github.com>
2021-03-21 20:14:37 -0700

    Update serializer.py

--- backend/Resources/serializer.py
+++ backend/Resources/serializer.py
@@ -4,4 +4,4 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ["Name","Emails","Phones","Organization","Notes"]#serializes all the fields in the Note model in models.py
+        fields = ["Name","Emails","Phones","Organization","Notes","id"]#serializes all the fields in the Note model in models.py
